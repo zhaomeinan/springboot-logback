3689bea279b75281ce9a89377103fc704d5ac5a0
jishu06 <zhaomeinan629omei@126.com>
2018-06-14 14:03:42 +0800

    上传源

---
+++ src/main/java/com/example/springbootdemo/HelloController.java
@@ -0,0 +1,25 @@
+package com.example.springbootdemo;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    private final Logger logger = LoggerFactory.getLogger(HelloController.class);
+
+    @RequestMapping("/hello")
+    public String hello() {
+
+        logger.trace("I am trace log.汉字");
+        logger.debug("I am debug log.汉字");
+        logger.info("I am info log.汉字");
+        logger.warn("I am warn log.汉字");
+        logger.error("I am error log.汉字");
+
+        int i = 1/0;
+        return "hello,this is a springboot demo";
+    }
+}
